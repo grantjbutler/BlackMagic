8fb72551590e866ef04d99e8bf329fc04c1fe791
Grant Butler <grantjbutler@users.noreply.github.com>
2018-12-09 11:49:52 -0500

    Add umbrella header.

---
+++ Sources/BlackMagic/include/BlackMagic/BlackMagic.h
@@ -0,0 +1,14 @@
+//
+//  BlackMagic.h
+//  BlackMagic
+//
+//  Created by Grant Butler on 12/9/18.
+//
+
+#ifndef BlackMagic_h
+#define BlackMagic_h
+
+#import <BlackMagic/BMDFunctions.h>
+#import <BlackMagic/BMDSwitcher.h>
+
+#endif /* BlackMagic_h */

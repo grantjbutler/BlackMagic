f43db343ba95ede35b1ca775ecc5480daaa682f5
Grant Butler <grantjbutler@users.noreply.github.com>
2020-01-08 12:46:37 -0500

    Add sample test to verify things are working.

--- Tests/BlackMagicTests/BlackMagicTests.swift
+++ Tests/BlackMagicTests/BlackMagicTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class BlackMagicTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BlackMagic().text, "Hello, World!")
+        XCTAssertTrue(BMDIsSDKInstalled())
     }
 
     static var allTests = [
